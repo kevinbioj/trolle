f4328b0ef5d64cd527cd3260aef1dd992a6f1f21
Kevin BIOJOUT <biojoutk1106@gmail.com>
2022-12-29 17:53:01 +0100

    fix lint error

--- frontend/src/core/api/auth.ts
+++ frontend/src/core/api/auth.ts
@@ -1,7 +1,7 @@
 import api from './_client';
 
 export const login = (username: string, password: string) =>
-  api<User, { username: string; password: string }>('/login', {
+  api<User, URLSearchParams>('/login', {
     body: new URLSearchParams({ username, password }),
   });
 
